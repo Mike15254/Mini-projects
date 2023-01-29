72fe3fa164a2f670fe9169f92e8d52377e96e55e
Mike15254 <mike8max10@gmail.com>
2023-01-29 23:16:41 +0300

    File: bug fix

--- Theme_switcher/app.js
+++ Theme_switcher/app.js
@@ -1,40 +1,40 @@
 // DOM Elements
 
-const darkButton = document.getElementById("dark");
-const lightButton = document.getElementById("light");
-const solarButton = document.getElementById("solar");
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const solarButton = document.getElementById('solar');
 const body = document.body;
 
 // Apply the cached theme on reload
 
-const theme = localStorage.getItem("theme");
-const isSolar = localStorage.getItem("isSolar");
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
 
 if (theme) {
   body.classList.add(theme);
-  isSolar && body.classList.add("solar");
+  isSolar && body.classList.add('solar');
 }
 
 // Button Event Handlers
 
 darkButton.onclick = () => {
-  body.classList.replace("light", "dark");
-  localStorage.setItem("theme", "dark");
+  body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
 };
 
 lightButton.onclick = () => {
-  body.classList.replace("dark", "light");
-  localStorage.setItem("theme", "light");
+  body.classList.replace('dark', 'light');
+  localStorage.setItem('theme', 'light');
 };
 
 solarButton.onclick = () => {
-  if (body.classList.contains("solar")) {
-    body.classList.remove("solar");
+  if (body.classList.contains('solar')) {
+    body.classList.remove('solar');
     solarButton.style.cssText = `
      --bg-solar: var(--yellow);
     `;
 
-    solarButton.innerText = "solarize";
+    solarButton.innerText = 'solarize';
 
     localStorage.removeItem(isSolar);
   } else {
@@ -42,9 +42,68 @@ solarButton.onclick = () => {
      --bg-solar: white;
     `;
 
-    body.classList.add("solar");
-    solarButton.innerText = "normalize";
+    body.classList.add('solar');
+    solarButton.innerText = 'normalize';
 
-    localStorage.setItem("isSolar", true);
+    localStorage.setItem('isSolar', true);
   }
+};
+
+var TxtRotate = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement('style');
+  css.type = 'text/css';
+  css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }';
+  document.body.appendChild(css);
 };
